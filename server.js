458182e3af6d34291df1d61de2ff7190605a93b3
Jon <jlee119955@gmail.com>
2025-03-21 10:53:56 -0400

    setup

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-import express from "express"
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('The server is running');
+  res.send('The server is running!');
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log(`Listening on port ${port}`);
+  console.log(`Your password is ${process.env.SECRET_PASSWORD}`);
 });
